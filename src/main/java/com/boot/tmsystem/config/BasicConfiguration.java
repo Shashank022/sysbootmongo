d8ce384126b599b5cf93bcaaf02c8481640a0d57
Shashank022 <shashank022@gmail.com>
2023-03-05 17:23:44 -0600

    Added changes to the code base...

--- src/main/java/com/boot/tmsystem/config/BasicConfiguration.java
+++ src/main/java/com/boot/tmsystem/config/BasicConfiguration.java
@@ -1,48 +1,52 @@
-package com.boot.tmsystem.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class BasicConfiguration {
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return encoder;
-    }
-}
+//package com.boot.tmsystem.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+//
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//                auth.inMemoryAuthentication()
+//                        .withUser("user")
+//                        .password(getPasswordEncoder().encode("pass"))
+//                        .roles("USER")
+//                        .and()
+//                        .withUser("admin")
+//                        .password("admin")
+//                        .roles("ADMIN");
+//    }
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                .antMatchers("/api/events").hasRole("user")
+//                .antMatchers("/api/tasks").hasRole("user")
+//                .antMatchers("/api/teams").hasRole("user")
+//                .antMatchers("/api/users").hasRole("user")
+//                .antMatchers("/api/user").hasRole("user")
+//                .antMatchers("/swagger-ui.html").hasRole("user")
+//                .antMatchers("/").permitAll()
+//                .and().formLogin();
+//
+//        super.configure(http);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder() {
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return encoder;
+//    }
+//}
