aa2010a0f69e6f32e69a8ddc79247c06b39a98a0
Shashank022 <shashank022@gmail.com>
2023-03-16 19:28:21 -0500

    Added exception handling changes

--- src/main/java/com/boot/tmsystem/exception/GlobalCheckedException.java
+++
@@ -1,8 +0,0 @@
-package com.boot.tmsystem.exception;
-
-public class GlobalCheckedException extends Exception{
-
-//    public IncorrectFileNameException(String errorMessage) {
-//        super(errorMessage);
-//    }
-}
